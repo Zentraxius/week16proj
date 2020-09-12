c873147a8c6a10a20d27a7a142380fc6abe50453
Cody Fritz <clanalia55@gmail.com>
2020-09-12 15:49:35 -0700

    commit to save progress

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import BarrelControl from "./BarrelControl";
+
+function App(){
+  return(
+<React.Fragment>
+  
+</React.Fragment>
+  );
+}
+export default App;
