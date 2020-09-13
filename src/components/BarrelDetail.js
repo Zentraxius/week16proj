900960a22c0ee68706187f5f35f9fe7daa2b4c7f
Cody Fritz <clanalia55@gmail.com>
2020-09-12 17:27:58 -0700

    update to save progress

--- src/components/BarrelDetail.js
+++ src/components/BarrelDetail.js
@@ -1,3 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-// export default BarrelDetail;
+function BarrelDetail(props){
+  const { barrel } = props;
+
+  return ( 
+    <React.Fragment>
+      <h1>Barrel Details</h1>
+      <h3>{barrel.name} by {barrel.brand}</h3>
+      price
+      pints
+      content
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+BarrelDetail.propTypes = {
+  barrel: PropTypes.object
+};
+
+export default BarrelDetail;
